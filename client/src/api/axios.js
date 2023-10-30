02cc39e64d8e68ad11b1ece81d2275e5c6b87335
AndreeGlz14 <jesusandree13@gmail.com>
2023-10-29 19:20:28 -0600

    Commit 7: conexion a la base de datos en la nube

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 //import 'dotenv/config';
 import axios from 'axios'
 
-const baseUrl = 'http://127.0.0.1:5050/api'
+const baseUrl = 'http://turing-ia.onrender.com/api'
 
 //Se crea una instancia para consumir la api
 const instance = axios.create({
